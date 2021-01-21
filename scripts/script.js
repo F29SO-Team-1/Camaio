a8670e688c1d9f7a1c6234d26fae565a3ca5bcaf
Callum Forsyth <43421946+cf51@users.noreply.github.com>
2021-01-21 16:20:28 +0000

    Adding event listners for the dark/mode buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,45 @@
+const toggleColorMode = e => {
+    // Switch to Light Mode
+    if (e.currentTarget.classList.contains("light--hidden")) {
+        // Sets the custom html attribute
+        document.documentElement.setAttribute("color-mode", "light");
+        // Sets the user's prefernce in local storage 
+        localStorage.setItem("color-mode", "light");
+        return; 
+    }
+
+    // Switch to Dark Mode
+    // Sets the custom html attribute 
+    document.documentElement.setAttribute("color-mode", "dark"); 
+    // Set the users prefernce in local storage 
+    localStorage.setItem("color-mode", "dark"); 
+};
+
+
+// Get the buttons in the DOM
+const toggleColorButtons = document.querySelectorAll(".color-mode_btn");
+
+// Set up even listeners
+toggleColorButtons.forEach(btn => {
+    btn.addEventListener("click", toggleColorMode)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
-    if (e.target.checked) {
+    if (e.currenttarget.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
     }
     else {
@@ -10,4 +48,6 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+
 
