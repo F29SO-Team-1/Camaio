3971a7f52946aefd62c820895c760fcea67e5573
Dominik <dominiksk09@gmail.com>
2021-01-31 17:00:52 +0000

    Updated the user and thier data, migration

--- Login/Migrations/20210131160340_UpdateProfile.Designer.cs
+++ Login/Migrations/20210131160340_UpdateProfile.Designer.cs
@@ -4,14 +4,16 @@ using Login.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Login.Migrations
 {
     [DbContext(typeof(LoginContext))]
-    partial class LoginContextModelSnapshot : ModelSnapshot
+    [Migration("20210131160340_UpdateProfile")]
+    partial class UpdateProfile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
