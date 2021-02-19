b234d559223bff4c140ba3b06144b5db39be0742
Dominik <dominiksk09@gmail.com>
2021-02-19 18:50:47 +0000

    Added the role contoller wich has the following functions; make new roles , delete roles , add user to role

--- Login/Controllers/RoleController.cs
+++ Login/Controllers/RoleController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Login.Areas.Identity.Data;
+using Login.Data;
+using Login.Models.ApplicationUser;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,12 +10,19 @@ using System.Threading.Tasks;
 
 namespace Login.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class RoleController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        public RoleController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<LoginUser> _userManager;
+        private readonly IApplicationUsers _userService;
+        public RoleController(RoleManager<IdentityRole> roleManager, 
+            IApplicationUsers userService , 
+            UserManager<LoginUser> userManager)
         {
             _roleManager = roleManager;
+            _userService = userService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -26,10 +37,69 @@ namespace Login.Controllers
         }
 
         [HttpPost]
+        [AutoValidateAntiforgeryToken]
         public async Task<IActionResult> CreateRole(IdentityRole role)
         {
             await _roleManager.CreateAsync(role);
             return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    return NotFound();
+            }
+            else
+                ModelState.AddModelError("", "No role found");
+            return View("Index");
+        }
+
+        public IActionResult AssignUser()
+        { 
+            var userModel = _userService.GetAll().Select(user => new ProfileModel
+            {
+                Username = user.UserName,
+                UserRating = user.Ratting,
+                Warnings = user.AccountWarnings
+            })
+                .ToList();
+
+            var userList = new ProfileModelList { ProfileList = userModel };
+
+            return View(userList);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> MakeAdmin()
+        {
+            await _userManager.AddToRoleAsync(await _userManager.GetUserAsync(User), "Admin");
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> MakeMod(string userId)
+        {
+            LoginUser user =  _userService.GetById(userId);
+            await _userManager.AddToRoleAsync(user, "Mod");
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> MakeUser(string userId)
+        {
+            //need to check if they are not a different role already
+            LoginUser user = _userService.GetById(userId);
+            await _userManager.AddToRoleAsync(user, "User");
+            return RedirectToAction("Index");
+        }
+
     }
 }
