2099bd6a5897470879f4d943a4df721ca4f1463b
Dewi-Payne <dewitbpayne@gmail.com>
2021-03-11 14:38:24 +0000

    Homepage display and styling updates

--- Login/wwwroot/js/site.js
+++ Login/wwwroot/js/site.js
@@ -11,6 +11,30 @@ $(document).ready(
 		$('.follow-button').css('background-color','green');
 		$('.follow-button').html('<div class="icon-ok"></div> Following');
 	});	
+	},
+	window.onload = function(){
+		//this orders the pictures on the homepage into columns
+		var pictures = document.getElementsByClassName("image-box");
+		const parent1 = document.getElementById("column-one");
+		const parent2 = document.getElementById('column-two');
+		const parent3 = document.getElementById('column-three');
+		const parent4 = document.getElementById('column-four');
+		for (var i = 0; i < pictures.length; i++) {
+			console.log("t");
+			if (i%4==0){
+				parent1.appendChild(pictures[i]);
+			}
+			else if (i%4==1){
+				parent2.appendChild(pictures[i]);
+			}
+			else if (i%4==2){
+				parent3.appendChild(pictures[i]);
+			}
+			else if (i%4==3){
+				parent4.appendChild(pictures[i]);
+			}
+		}
 	}
 
 );
+
